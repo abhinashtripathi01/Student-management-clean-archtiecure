d13349a4ba5db57e915f514afab0e027f3051b6d
abhinashtripathi01 <abhinash8848@gmail.com>
2024-06-09 11:23:49 +0545

    update all error

--- lib/features/batch/data/repository/batch_repository.dart
+++ lib/features/batch/data/repository/batch_repository.dart
@@ -1,23 +1,27 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:student_management_starter/core/failure/failure.dart';
+import 'package:student_management_starter/features/batch/data/data_source/local/batch_local_datasource.dart';
 import 'package:student_management_starter/features/batch/domain/entity/batch_entity.dart';
 import 'package:student_management_starter/features/batch/domain/repository/batch_repository.dart';
 
-final BatchLocalRepository = Provider<IBatchRepository>((ref) {
-  return BatchLocalRepository();
+final batchLocalRepository = Provider<IBatchRepository>((ref) {
+  return BatchLocalRepository(
+    batchLocalDatasource: ref.read(batchLocalDatasourceProvider),
+  );
 });
 
 class BatchLocalRepository implements IBatchRepository {
+  final BatchLocalDatasource batchLocalDatasource;
+
+  BatchLocalRepository({required this.batchLocalDatasource});
   @override
   Future<Either<Failure, bool>> addBatch(BatchEntity batch) {
-    // TODO: implement addBatch
-    throw UnimplementedError();
+    return batchLocalDatasource.addBatch(batch);
   }
 
   @override
   Future<Either<Failure, List<BatchEntity>>> getAllBatches() {
-    // TODO: implement getAllBatches
-    throw UnimplementedError();
+    return batchLocalDatasource.getAllBatches();
   }
 }
